029eb588f0bebf2cb81975b077f65e0d75f2615f
Michael Kaszynski <mkaszynski2000@gmail.com>
2022-04-02 10:15:54 +0200

    refactor: use main function

--- gravity.py
+++ gravity.py
@@ -9,24 +9,24 @@ Controls:
     - "Left click" : Place an object
     - "Right click" : Remove an object
 
+TODO:
+- Add argsparse
+- fix moving camera
+- fix drift
+
 """
 
 import random
 
 import pygame
 
-pygame.init()
 
-
-def add_line(screen, text, x, y):
+def add_line(font, screen, text, x, y):
     # used to print the status of the variables
     text = font.render(text, True, (255, 255, 255))
     text_rect = text.get_rect()
     text_rect.topleft = (x, y)
     screen.blit(text, text_rect)
-
-
-font = pygame.font.Font("freesansbold.ttf", 32)
 
 
 def distance(pos1, pos2):
@@ -37,196 +37,177 @@ def distance(pos1, pos2):
 
 
 INIT_VEL_FAC = 3
-
-pause = False
-
 GRAVITY_CONST = 0.0005
 MAX_OBJECTS = 150
-
-time2 = 2022
-
-mass = 1
-
-# Set up the drawing window
-x_width, y_width = 1200, 650
-screen = pygame.display.set_mode([x_width, y_width])
-dire = ""
-
 MIN_VAL = 1
 
-clock = pygame.time.Clock()
-
-# these are the gravitational objects
-objects = []
-
-# optionally initialize with random objects
-# for i in range(0, 100):
-#     objects.append([random.random()*650, random.random()*650, random.random()*2*INIT_VEL_FAC - INIT_VEL_FAC, random.random()*2*INIT_VEL_FAC - INIT_VEL_FAC, 1, (random.random()*255, (random.random()*255, (random.random()*255)])
 
 
 # Run until the user asks to quit
-running = True
-while running:
+def main():
 
-    pygame.event.poll()
-    keys = pygame.key.get_pressed()
+    # initialize pygame and the font
+    pygame.init()
+    font = pygame.font.Font("freesansbold.ttf", 32)
+    clock = pygame.time.Clock()
 
-    if keys[pygame.K_p]:
-        pause = True
-    if keys[pygame.K_o]:
-        pause = False
+    pause = False
+    time2 = 2022
 
-    # global center of gravity
-    sum1 = 1e-5
-    add1 = 1e-5
-    sum2 = 1e-5
-    cg = (sum1 / add1, sum2 / add1)
+    # Set up the drawing window
+    x_width, y_width = 1200, 650
+    screen = pygame.display.set_mode([x_width, y_width])
+    dire = ""
 
-    # increase or decrease mass if w or s is held
-    if keys[pygame.K_w]:
-        mass += 0.1
-    if keys[pygame.K_s]:
-        mass -= 0.1
+    # these are the gravitational objects
+    objects = []
 
-    time2 += 0.05
+    # optionally initialize with random objects
+    # for i in range(0, 100):
+    #     objects.append([random.random()*650, random.random()*650, random.random()*2*INIT_VEL_FAC - INIT_VEL_FAC, random.random()*2*INIT_VEL_FAC - INIT_VEL_FAC, 1, (random.random()*255, (random.random()*255, (random.random()*255)])
 
-    if mass < 1:
-        mass = 1
+    mass = 1
+    objects = []
 
-    mouse_held = pygame.mouse.get_pressed()
-    mx, my = pygame.mouse.get_pos()
+    running = True
+    while running:
 
-    # Remove an object when the right mouse button is held down
-    if mouse_held[2]:
-        # loop through all objects and check if within 20 pixels
-        for i in objects:
-            if (
-                i[0] > (mx + cg[0] - x_width // 2) - 10
-                and i[0] < (mx + cg[0] - x_width // 2) + 10
-            ):
+        pygame.event.poll()
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_p]:
+            pause = True
+        if keys[pygame.K_o]:
+            pause = False
+
+        # global center of gravity
+        sum1 = 1e-5
+        add1 = 1e-5
+        sum2 = 1e-5
+        cg = (sum1 / add1, sum2 / add1)
+
+        # increase or decrease mass if w or s is held
+        if keys[pygame.K_w]:
+            mass += 0.1
+        if keys[pygame.K_s]:
+            mass -= 0.1
+
+        time2 += 0.05
+
+        if mass < 1:
+            mass = 1
+
+        mouse_held = pygame.mouse.get_pressed()
+        mx, my = pygame.mouse.get_pos()
+
+        # Remove an object when the right mouse button is held down
+        if mouse_held[2]:
+            # loop through all objects and check if within 20 pixels
+            for i in objects:
                 if (
-                    i[1] > (my + cg[0] - y_width // 2) - 10
-                    and i[1] < (my + cg[0] - y_width // 2) + 10
+                    i[0] > (mx + cg[0] - x_width // 2) - 10
+                    and i[0] < (mx + cg[0] - x_width // 2) + 10
                 ):
-                    objects.remove(i)
+                    if (
+                        i[1] > (my + cg[0] - y_width // 2) - 10
+                        and i[1] < (my + cg[0] - y_width // 2) + 10
+                    ):
+                        objects.remove(i)
 
-    # Add an object when the left mouse button is held down
-    if mouse_held[0]:
-        objects.append(
-            [
-                mx + cg[0] - x_width // 2,  # x position
-                my + cg[1] - y_width // 2,  # y position
-                0,  # x velocity
-                0,  # y velocity
-                mass,  # mass
-                random.random() * 255,  # red
-                random.random() * 255,  # green
-                random.random() * 255,  # blue
-            ]
-        )
+        # Add an object when the left mouse button is held down
+        if mouse_held[0]:
+            objects.append(
+                [
+                    mx + cg[0] - x_width // 2,  # x position
+                    my + cg[1] - y_width // 2,  # y position
+                    0,  # x velocity
+                    0,  # y velocity
+                    mass,  # mass
+                    random.random() * 255,  # red
+                    random.random() * 255,  # green
+                    random.random() * 255,  # blue
+                ]
+            )
 
-    if not pause:
-        for i, obj_a in enumerate(objects):
-            for obj_b in objects:
+        if not pause:
+            for i, obj_a in enumerate(objects):
+                for obj_b in objects:
 
-                # skip if self
-                if obj_a is obj_b:
-                    continue
+                    # skip if self
+                    if obj_a is obj_b:
+                        continue
 
-                xdist = obj_b[0] - obj_a[0]
+                    xdist = obj_b[0] - obj_a[0]
 
-                if abs(xdist) < MIN_VAL:
+                    if abs(xdist) < MIN_VAL:
+                        if xdist < 0:
+                            xdist = -MIN_VAL
+                        else:
+                            xdist = MIN_VAL
+
+                    ydist = obj_b[1] - obj_a[1]
+                    if abs(ydist) < MIN_VAL:
+                        if ydist < 0:
+                            ydist = -MIN_VAL
+                        else:
+                            ydist = MIN_VAL
+
+                    # compute force of gravity in x direction
+                    x_gravity = (
+                        obj_a[4] * obj_b[4] * GRAVITY_CONST * (xdist) ** -2
+                        + obj_a[4] * GRAVITY_CONST
+                    )
                     if xdist < 0:
-                        xdist = -MIN_VAL
+                        obj_b[2] += x_gravity
                     else:
-                        xdist = MIN_VAL
+                        obj_b[2] -= x_gravity
 
-                ydist = obj_b[1] - obj_a[1]
-                if abs(ydist) < MIN_VAL:
+                    # compute force of gravity in y direction
+                    y_gravity = (
+                        obj_a[4] * obj_b[4] * GRAVITY_CONST * (ydist) ** -2
+                        + obj_a[4] * GRAVITY_CONST
+                    )
                     if ydist < 0:
-                        ydist = -MIN_VAL
+                        obj_b[3] += y_gravity
                     else:
-                        ydist = MIN_VAL
+                        obj_b[3] -= y_gravity
 
-                # compute force of gravity in x direction
-                x_gravity = (
-                    obj_a[4] * obj_b[4] * GRAVITY_CONST * (xdist) ** -2
-                    + obj_a[4] * GRAVITY_CONST
-                )
-                if xdist < 0:
-                    obj_b[2] += x_gravity
-                else:
-                    obj_b[2] -= x_gravity
+            for i in objects:
+                sum1 += i[0]
+                add1 += i[4]
+                sum2 += i[1]
 
-                # compute force of gravity in y direction
-                y_gravity = (
-                    obj_a[4] * obj_b[4] * GRAVITY_CONST * (ydist) ** -2
-                    + obj_a[4] * GRAVITY_CONST
-                )
-                if ydist < 0:
-                    obj_b[3] += y_gravity
-                else:
-                    obj_b[3] -= y_gravity
+            # Did the user click the window close button?
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-        for i in objects:
-            sum1 += i[0]
-            add1 += i[4]
-            sum2 += i[1]
+            if len(objects) > MAX_OBJECTS:
+                objects.remove(random.choice(objects))
 
-        # Did the user click the window close button?
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            # Fill the background with black
+            screen.fill((0, 0, 0))
+            add_line(font, screen, f"mass {mass:.0f}", 0, 0)
 
-        if len(objects) > MAX_OBJECTS:
-            objects.remove(random.choice(objects))
+            # update the position from velocity
+            for g in objects:
+                g[0] += g[2]  # x
+                g[1] += g[3]  # y
 
-        # Fill the background with black
-        screen.fill((0, 0, 0))
-        add_line(screen, f"mass {mass:.0f}", 0, 0)
+        for u in objects:
+            debris = pygame.Rect(
+                u[0] - cg[0] + x_width // 2, u[1] - cg[1] + y_width // 2, u[4] * 2, u[4] * 2
+            )
+            pygame.draw.rect(screen, (u[5], u[6], u[7]), debris)
 
-        # update the position from velocity
-        for g in objects:
-            g[0] += g[2]  # x
-            g[1] += g[3]  # y
+        clock.tick(60)
 
-            # objects.append((g[0] + g[2]*0.01, g[1] - g[3]*0.01, g[2], g[3], g[4]))
-        # print(len(objects))
+        # Flip the display
+        pygame.display.flip()
 
-        # for o in objects:
-        #     if o[0] > 600:
-        #         o[0] = 600
-        #         o[2]*=-1
-        #     if o[0] < 0:
-        #         o[0] = 0
-        #         o[2]*=-1
-        #     if o[1] > 600:
-        #         o[1] = 600
-        #         o[3]*=-1
-        #     if o[1] < 0:
-        #         o[1] = 0
-        #         o[3]*=-1
+    # Done! Time to quit.
+    pygame.quit()
 
-        # for x in objects:
-        #     for y in objects:
-        #         dis = distance((x[0], x[1]), (y[0], y[1]))
-        #         if x[0] == y[0] and x[1] == y[1]:
-        #             if x in objects:
-        #                 objects.remove(x)
-        #             if y in objects:
-        #                 objects.remove(y)
-        #             objects.append([x[0], x[1], 0, 0, (x[4] + y[4])*0.501])
 
-    for u in objects:
-        debris = pygame.Rect(
-            u[0] - cg[0] + x_width // 2, u[1] - cg[1] + y_width // 2, u[4] * 2, u[4] * 2
-        )
-        pygame.draw.rect(screen, (u[5], u[6], u[7]), debris)
-
-    clock.tick(60)
-
-    # Flip the display
-    pygame.display.flip()
-
-# Done! Time to quit.
-pygame.quit()
+if __name__ == '__main__':
+    main()
